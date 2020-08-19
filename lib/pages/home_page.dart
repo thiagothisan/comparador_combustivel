bf703524d02b8fde22fc95619e81e88ba890a154
thisan <thiago.thisan@gmail.com>
2020-08-19 12:09:44 -0300

    criando input_widgett

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,12 @@
+import 'package:comparador_combustivel/widgets/input_widget.dart';
 import 'package:comparador_combustivel/widgets/logo_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
 
 class HomePage extends StatelessWidget {
+  var _gasCtrl = new MoneyMaskedTextController();
+  var _alcCtrl = new MoneyMaskedTextController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,6 +14,14 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           LogoWidget(),
+          InputWidget(
+            label: "Alcool",
+            ctrl: _alcCtrl,
+          ),
+          InputWidget(
+            label: "Gasolina",
+            ctrl: _gasCtrl,
+          ),
         ],
       ),
     );
