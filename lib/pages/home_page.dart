55f015226cf72b74105005b9ff13968ec5e0eab8
thisan <thiago.thisan@gmail.com>
2020-08-19 11:51:47 -0300

    criando logo_widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,16 @@
+import 'package:comparador_combustivel/widgets/logo_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: ListView(
+        children: <Widget>[
+          LogoWidget(),
+        ],
+      ),
+    );
   }
 }
